d6ffab3b2255a535928c4f6f5bd58a1d09e92a4f
Fabiana Dantas <Fabi@MacBook-Air-de-Usuario.local>
2020-06-01 00:43:45 -0300

    Mensagens de erro utilizando alertify

--- WebDatingApp-SPA/src/app/nav/nav.component.ts
+++ WebDatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,27 +10,28 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
 
   model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
     this.authService.login(this.model).subscribe(next => {
-      console.log('Login efetuado com sucesso!');
+      this.alertify.success('Login efetuado com sucesso!');
     }, error => {
-      console.log('Erro Login');
+      this.alertify.error(error);
     });
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
+    // const token = localStorage.getItem('token');
     // a exclamaçao dupla seta a variavel como boolean, retornando true se estiver preenchida e false caso contrario
-    return !!token;
+    // return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('Log out');
+    this.alertify.message('Log out');
   }
 }
--- WebDatingApp-SPA/src/app/register/register.component.ts
+++ WebDatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -11,23 +12,23 @@ export class RegisterComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register() {
     this.authService.register(this.model).subscribe(() => {
-      console.log('sucesso registrando');
+      this.alertify.success('Registro efetuado com sucesso.');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   cancel() {
     // estamos emitindo um boolean false para a classe pai (home), com output
     this.cancelRegister.emit(false);
-    console.log('#canceladah');
+    this.alertify.warning('Registro cancelado');
   }
 
 }
